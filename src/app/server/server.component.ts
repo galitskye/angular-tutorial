fb6ec05217340daa3bb41163cd8bb322aefa9efc
galitsky <eugen.galitsky@spryker.com>
2020-02-04 13:30:49 +0200

    T-2: servers for

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-server',
+    styleUrls: ['./server.component.scss'],
     templateUrl: './server.component.html'
 })
 
 export class ServerComponent {
-    serverId: number = 10;
+	@Input() serverId: any = 10;
     serverStatus: string = 'offline';
+
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
 
     getServerStatus(): string {
         return this.serverStatus;
+    }
+
+    getColor(): string {
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 }
